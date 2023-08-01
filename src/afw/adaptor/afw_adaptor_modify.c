f12e0dda2ee99dfedc781fdf715d63d1ee52e8a5
Mike Gossett <mike@clemson.edu>
2023-08-01 22:12:49 +0000

    #36 Fix scan error in afw_adaptor_modify.c (was not logic error)

--- src/afw/adaptor/afw_adaptor_modify.c
+++ src/afw/adaptor/afw_adaptor_modify.c
@@ -129,6 +129,10 @@ afw_adaptor_modify_entry_type(
         entry_type[i] && !afw_utf8_equal(entry_type[i], mnemonic);
         i++);
 
+    if (i > afw_adaptor_modify_entry_type_invalid) {
+        i = afw_adaptor_modify_entry_type_invalid;
+    }
+
     return (entry_type[i]) ? i : afw_adaptor_modify_entry_type_invalid;
 }
 
