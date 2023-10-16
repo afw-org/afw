38aac6f76caf4a46af414f9085de577c7218fd44
Jeremy Grieshop <jeremygrieshop@gmail.com>
2023-10-16 13:34:38 +0000

    Fix for #67

--- src/afw_components/react/core/src/layouts/dataTypes/Template/Template.js
+++ src/afw_components/react/core/src/layouts/dataTypes/Template/Template.js
@@ -8,6 +8,7 @@
 
 import {
     CodeEditor,
+    TextField,
     Typography,
     useOperational,
     useTheme,
@@ -23,35 +24,49 @@ export const Template = (props) => {
     const theme = useTheme();
     const {editable} = useOperational(props);
                
-    const {id, value, valueMeta = {}, onChanged} = props;
+    const {id, value, valueMeta = {}, autoFocus, onChanged} = props;
     const {label, brief} = valueMeta;
 
     return (
-        <ExpandableComponent>
-            <div style={{ display: "flex", flexDirection: "column", height: "100%", minHeight: "100px" }}>
-                {
-                    editable && label &&
-                        <Typography style={{ marginBottom: theme.spacing(0.5) }} size="2" color="textSecondary" text={label} />
-                }
-                <CodeEditor 
-                    style={{ flex: 1, height: "100%" }}
-                    id={id}
-                    label={label}
-                    // eslint-disable-next-line jsx-a11y/no-autofocus
-                    autoFocus={true}
-                    readOnly={!editable}
-                    source={value}
-                    language="afw"
-                    wordWrap={true}
-                    showMinimap={false}
-                    onChanged={onChanged}
-                    showGutter={false}
-                />
-                {
-                    editable && brief &&
-                        <Typography style={{ marginTop: theme.spacing(0.5) }} size="1" text={brief} />
-                }
-            </div>
+        <ExpandableComponent
+            expandedChildren={
+                <div style={{ display: "flex", flexDirection: "column", height: "100%", minHeight: "100px" }}>
+                    {
+                        editable && label &&
+                            <Typography style={{ marginBottom: theme.spacing(0.5) }} size="2" color="textSecondary" text={label} />
+                    }
+                    <CodeEditor 
+                        style={{ flex: 1, height: "100%" }}
+                        id={id}
+                        label={label}
+                        // eslint-disable-next-line jsx-a11y/no-autofocus
+                        autoFocus={true}
+                        readOnly={!editable}
+                        source={value}
+                        language="afw"
+                        wordWrap={true}
+                        showMinimap={false}
+                        onChanged={onChanged}
+                        showGutter={false}
+                    />
+                    {
+                        editable && brief &&
+                            <Typography style={{ marginTop: theme.spacing(0.5) }} size="1" text={brief} />
+                    }
+                </div>
+            }
+        >
+            <TextField 
+                id={id}                
+                // eslint-disable-next-line jsx-a11y/no-autofocus
+                autoFocus={autoFocus}
+                {...valueMeta}
+                description={brief}
+                type="text"
+                value={value}
+                multiline={true}           
+                onChanged={onChanged}  
+            /> 
         </ExpandableComponent>
     );
 };
